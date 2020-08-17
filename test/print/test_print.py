b62ec7c09c702a5e0d3542f7ea2253fcc5a9b323
Peterlits Zo <peterlitszo@outlook.com>
2020-08-17 23:22:41 +0800

    test: (print) test about print

--- test/print/test_print.py
+++ test/print/test_print.py
@@ -9,4 +9,54 @@ class TestPrint(unittest.TestCase):
         >>> print, "this is the string..."
         ... this is the string...
         """
-        self.fail('waiting...')
+        self.assertEqual(
+            run('print, "this is the string..."'),
+            "this is the string..."
+        )
+        # the white space do not make the result different
+        self.assertEqual(
+            run('     print   ,     "this is the string..."       '),
+            "this is the string..."
+        )
+        # but endiff result if in string:
+        self.assertEqual(
+            run('print, "this    is the string..."'),
+            "this     is the string..."
+        )
+
+    def test_print_2(self):
+        """
+        There is two way to show the string: "xxx" and 'xxx', like python.
+        """
+        self.assertEqual(
+            run('''print, "this is the 'string'!" '''),
+            "this is the 'string'!"
+        )
+        self.assertEqual(
+            run('''print, 'this is the "string"!' '''),
+            "this is the 'string'!"
+        )
+
+    def test_print_3(self):
+        """
+        It can escape some characters: \" or \' and \\
+        """
+        self.assertEqual(
+            run(r'print, "\"\""'),
+            '""'
+        )
+        self.assertEqual(
+            run(r"print, '\'\''"),
+            "''"
+        )
+        self.assertEqual(
+            run(r"print, '\\'"),
+            "\\"
+        )
+
+    def test_print_4(self):
+        # if the character is after `\` sign, just output what it is.
+        self.assertEqual(
+            run("print, '\a\b\c'"),
+            'abc'
+        )
