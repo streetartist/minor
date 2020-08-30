68ca1e1fe9dae7e736af911c57228171dca28cfe
streetartist <60282562+streetartist@users.noreply.github.com>
2020-08-30 14:02:24 +0800

    Use Rply to make a new Minor

--- minor.py
+++ minor.py
@@ -1,8 +1,13 @@
 from src import main
 
+'''
 while True:
     input_ = input(">>> ")
     if input_ != "$quit":
         print("...", main.run(input_))
     else:
         break
+'''
+
+while(True):
+    print(parser.parse(lexer.lex(input(">>>"))).eval())
